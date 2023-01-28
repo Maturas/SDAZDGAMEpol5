a7fc46bb4c6809c090e9cb1917050a4dbdb2aef1
Mateusz Zawistowski <maturasisko@gmail.com>
2023-01-28 13:25:46 +0100

    Implemented primitive camera following player

---
+++ Assets/Source/GameLogic/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace SDAZDGAMEpol5.GameLogic
+{
+    public class CameraController : MonoBehaviour
+    {
+        [field: SerializeField]
+        private Transform FollowTarget { get; set; }
+
+        private void Update()
+        {
+            if (FollowTarget == null)
+                return;
+
+            // Primitive 1:1 camera follow movement implementation
+            var targetPosition = FollowTarget.position;
+            targetPosition.z = transform.position.z;
+            transform.position = targetPosition;
+        }
+    }
+}
