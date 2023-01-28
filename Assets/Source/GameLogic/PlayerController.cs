19eed80f4940ca17870d3c108be2dd9afa209b09
Mateusz Zawistowski <maturasisko@gmail.com>
2023-01-28 11:04:21 +0100

    Player Controller progress - basic movement and jumping without groundcheck implemented

---
+++ Assets/Source/GameLogic/PlayerController.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+namespace SDAZDGAMEpol5.GameLogic
+{
+    public class PlayerController : MonoBehaviour
+    {
+        // public float MovementAcceleration; // public field, serialized by default
+        
+        // [SerializeField] // how to serialize a private field
+        // private float MovementAcceleration; // private field
+        
+        [field: SerializeField] // how to serialize a property
+        private float MovementAcceleration { get; set; } // Property
+        
+        [field: SerializeField]
+        private float JumpAcceleration { get; set; }
+        
+        private Rigidbody2D Rigid { get; set; }
+        
+        private float MoveDirection { get; set; } // -1 - move left, 1 - move right, 0 - don't move
+        private bool ShouldJump { get; set; }
+        
+        private void Start()
+        {
+            // Invoked once, at the beginning of this GameObject's life cycle
+            Rigid = GetComponent<Rigidbody2D>(); // NEVER use GetComponent on Update or FixedUpdate!!!
+        }
+
+        private void Update()
+        {
+            // Invoked every frame, warning: FPS varies per device/system/etc
+            if (Input.GetKey(KeyCode.A))
+            {
+                MoveDirection = -1.0f;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                MoveDirection = 1.0f;
+            }
+            else
+            {
+                MoveDirection = 0.0f;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ShouldJump = true;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            // Invoked every physics engine refresh, constant amount of refreshes per second on every device/system/etc
+            
+            // GetKey - returns true while the key is pressed down (continuously)
+            // GetKeyDown - returns true on one frame, when the user has pressed the key
+            // GetKeyUp - returns true on one frame, when the user has released the key
+
+            // Horizontal movement
+            Rigid.AddForce(Vector2.right * (MovementAcceleration * MoveDirection), ForceMode2D.Force);
+
+            if (ShouldJump)
+            {
+                // Jump
+                Rigid.AddForce(Vector2.up * JumpAcceleration, ForceMode2D.Impulse);
+
+                ShouldJump = false;
+            }
+        }
+    }   
+}
