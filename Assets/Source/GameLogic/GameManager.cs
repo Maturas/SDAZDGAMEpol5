6d9ac1280bc279ef27bcb3c85345e9671f4e40be
Mateusz Zawistowski <maturasisko@gmail.com>
2023-01-29 15:29:55 +0100

    Added Singleton example

---
+++ Assets/Source/GameLogic/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace SDAZDGAMEpol5.GameLogic
+{
+    /// <summary>
+    ///     This class implements Singleton design pattern - single, globally available instance, which is created
+    ///     at the start of the program and destroyed at the end. USE WITH CAUTION!
+    /// </summary>
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance { get; private set; }
+
+        /// <summary>
+        ///     Awake executes before Start
+        /// </summary>
+        private void Awake()
+        {
+            if (Instance)
+            {
+                Debug.LogError("Found another instance of GameManager! Destroying!");
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Source/GameLogic/Main.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SDAZDGAMEpol5.GameLogic
+{
+    public class Main : MonoBehaviour
+    {
+        [field: SerializeField]
+        private int LevelToLoadIndex { get; set; }
+        
+        private void Start()
+        {
+            SceneManager.LoadScene(LevelToLoadIndex);
+        }
+    }
+}
